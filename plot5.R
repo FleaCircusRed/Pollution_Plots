22ee64d2861114830a5fe493406812489c4b3eb1
Barry ODriscoll <fleacircusred@gmail.com>
2014-10-23 23:44:15 +0100

    added plot 5

---
+++ plot5.R
@@ -0,0 +1,37 @@
+# requires ggplot2
+library(ggplot2)
+
+# load data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# Question 5 asks to plot the Emissions from Motor vehicles in Baltimore
+# I take Motor vehicles to mean mean self propelled on road vehicles that does 
+# use rails excluding off road vehicles and heavy construction.  This is based
+# upon the wikipedia article for motor vehicles which contains the following 
+# quote:
+#
+# "The U.S. publisher Ward's, estimate that as of 2010 there were 1.015 billion 
+# motor vehicles in use in the world. This figure represents the number of cars; 
+# light, medium and heavy duty trucks; and buses, but does not include off-road 
+# vehicles or heavy construction equipment."
+
+# get baltimore emissions data
+baltimore <- subset(NEI, fips == "24510")
+
+# get onroad motor vehicle sources
+on_road_sources <- subset(SCC, Data.Category == "Onroad")
+
+# filter baltimore emission data by motor vehicle sources
+baltimore_vehicle_emissions <- subset(baltimore, baltimore$SCC %in% on_road_sources$SCC)
+
+# get the sum for each year of emissions
+totals <- aggregate(Emissions~year, data=baltimore_vehicle_emissions, sum)
+
+# plot and save the emissions for motor vehicles by year
+p <- ggplot(totals, aes(year, Emissions)) + geom_point() + geom_line() +
+    labs(title="Baltimore Emissions caused by Motor Vehicles") + 
+    ylab(expression("Total Emissions PM"[2.5])) +
+    xlab("Year")
+
+ggsave("plot5.png", p)
