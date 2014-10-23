3df4a155d3a49bf582c6107db24bac6aedf87b8a
Barry ODriscoll <fleacircusred@gmail.com>
2014-10-23 22:57:06 +0100

    improved comments in plot 3 and finished plot 4

--- plot3.R
+++ plot3.R
@@ -5,15 +5,18 @@ library(ggplot2)
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-# get total emmissions by year and by type for baltimore
+# get total emmissions by year and by type for Baltimore
 baltimore <- subset(NEI, fips == "24510")
 totals <- aggregate(Emissions~year+type, data=baltimore, sum)
+
+#tidy up type column
 totals$type[totals$type == "NON-ROAD"] <- "Off-Road"
 totals$type[totals$type == "ON-ROAD"] <- "On-Road"
 totals$type[totals$type == "POINT"] <- "Point"
 totals$type[totals$type == "NONPOINT"] <- "Non-Point"
 totals$type <- as.factor(totals$type)
 
+# plot total emissions by year and type for Baltimore
 p <- ggplot(totals, aes(year, Emissions, group = type, color = type) 
             ) + geom_point() + geom_line() + 
     labs(title="Baltimore Total Emissions by Type") + 
---
+++ plot4.R
@@ -0,0 +1,29 @@
+# requires ggplot2
+library(ggplot2)
+
+# load data
+#NEI <- readRDS("summarySCC_PM25.rds")
+#SCC <- readRDS("Source_Classification_Code.rds")
+
+# Question 4 asks to show data for all coal combustion-related sources
+# I've taken this to mean all sources that's shortname contain coal and
+# comb or combustion
+
+coal <- grepl("Coal", SCC$Short.Name, ignore.case = TRUE)
+comb <- grepl("Comb |Combustion", SCC$Short.Name,  ignore.case = TRUE)
+
+coal_combustion <- SCC[coal & comb, ]
+
+#get the subset of Emissions data that come from coal combustion
+coal_emissions <- subset(NEI, NEI$SCC %in% coal_combustion$SCC)
+
+#group emission totals by year
+coal_totals <- aggregate(Emissions~year, data=coal_emissions, sum)
+
+#plot and save graph
+p <- ggplot(coal_totals, aes(year, Emissions)) + geom_point() + geom_line() +
+    labs(title="Nationwide Emissions caused by Coal Combustion") + 
+    ylab(expression("Total Emissions PM"[2.5])) +
+    xlab("Year")
+
+ggsave("plot4.png", p)
