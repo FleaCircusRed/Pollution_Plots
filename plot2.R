84d8fcea6ddd6b89a3d3202b58c772e144a70a1c
Barry ODriscoll <fleacircusred@gmail.com>
2014-10-23 19:27:01 +0100

    added plot 2

---
+++ plot2.R
@@ -0,0 +1,18 @@
+# load data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# get total emmissions by year for baltimore
+baltimore <- subset(NEI, fips == "24510")
+baltimore <- rowsum(baltimore$Emissions, baltimore$year)
+baltimore <- cbind(baltimore, c(1999,2002,2005,2008))
+
+
+# generate plot as png
+png(filename = "plot2.png")
+
+plot(baltimore[,2], baltimore[,1], type = "l", 
+     ylab = expression('Total Emissions - PM '[2.5]), xlab = "Year")
+title(main = "Baltimore, Maryland")
+
+dev.off()
